277f1bd4c8268acbc985088a85eec05522b2ad52
José Núñez <79015124+josiext@users.noreply.github.com>
2023-03-28 10:44:16 -0300

    refactor

--- src/pages/api/project-gpt.ts
+++ src/pages/api/project-gpt.ts
@@ -1,6 +1,53 @@
-import { Project } from "@/stores/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
+
+const messages = [
+  {
+    role: "system",
+    content:
+      "Asume que eres un creador de intrucciones. El usuario te pasará cosas que quiere hacer y tu le respondes con esas instrucciones.",
+  },
+  {
+    role: "user",
+    content:
+      "Crea un proyecto con el nombre de: BECO y sus amigos. Y con una descripcion: BECO y sus amigos es un proyecto de desarrollo de videojuegos.",
+  },
+  {
+    role: "assistant",
+    content:
+      "create|BECO y sus amigos|BECO y sus amigos es un proyecto de desarrollo de videojuegos",
+  },
+  {
+    role: "user",
+    content:
+      "Crea un proyecto con el nombre de: BECO y sus amigos. Y con una descripcion: BECO y sus amigos es un proyecto de desarrollo de videojuegos.",
+  },
+  {
+    role: "assistant",
+    content:
+      "create|BECO y sus amigos|BECO y sus amigos es un proyecto de desarrollo de videojuegos",
+  },
+  {
+    role: "user",
+    content:
+      "Crea un proyecto con el nombre hacer demandas para provida cuya descripcion es Provida necesita que le hagamos demandas a sus clientes.",
+  },
+  {
+    role: "assistant",
+    content:
+      "create|hacer demandas para provida|Provida necesita que le hagamos demandas a sus clientes",
+  },
+  {
+    role: "user",
+    content:
+      "Nuevo proyecto, nombre: hacer demandas a Consultorio las Polvoras. Descripcion: EL consultorio se ha portado muy mal, tiene muchas quejas.",
+  },
+  {
+    role: "assistant",
+    content:
+      "create|hacer demandas a Consultorio las Polvoras|EL consultorio se ha portado muy mal, tiene muchas quejas",
+  },
+] as const;
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -27,54 +74,6 @@ export default async function handler(
   if (!prompt) {
     return res.status(400).end();
   }
-
-  const messages = [
-    {
-      role: "system",
-      content:
-        "Asume que eres un creador de intrucciones. El usuario te pasará cosas que quiere hacer y tu le respondes con esas instrucciones.",
-    },
-    {
-      role: "user",
-      content:
-        "Crea un proyecto con el nombre de: BECO y sus amigos. Y con una descripcion: BECO y sus amigos es un proyecto de desarrollo de videojuegos.",
-    },
-    {
-      role: "assistant",
-      content:
-        "create|BECO y sus amigos|BECO y sus amigos es un proyecto de desarrollo de videojuegos",
-    },
-    {
-      role: "user",
-      content:
-        "Crea un proyecto con el nombre de: BECO y sus amigos. Y con una descripcion: BECO y sus amigos es un proyecto de desarrollo de videojuegos.",
-    },
-    {
-      role: "assistant",
-      content:
-        "create|BECO y sus amigos|BECO y sus amigos es un proyecto de desarrollo de videojuegos",
-    },
-    {
-      role: "user",
-      content:
-        "Crea un proyecto con el nombre hacer demandas para provida cuya descripcion es Provida necesita que le hagamos demandas a sus clientes.",
-    },
-    {
-      role: "assistant",
-      content:
-        "create|hacer demandas para provida|Provida necesita que le hagamos demandas a sus clientes",
-    },
-    {
-      role: "user",
-      content:
-        "Nuevo proyecto, nombre: hacer demandas a Consultorio las Polvoras. Descripcion: EL consultorio se ha portado muy mal, tiene muchas quejas.",
-    },
-    {
-      role: "assistant",
-      content:
-        "create|hacer demandas a Consultorio las Polvoras|EL consultorio se ha portado muy mal, tiene muchas quejas",
-    },
-  ] as const;
 
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
